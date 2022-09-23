fe070380f45ef71bf217abe6de84021aa1991c83
Vlad-2 <test@example.com>
2022-09-23 19:13:55 +0300

    rout

--- src/index.js
+++ src/index.js
@@ -4,3 +4,23 @@ const add123 = (a, b) => a + b;
 
 const res = add123(222, 3);
 console.log("Helloasda");
+
+
+
+/*for (let i = '#'; i.length < 8; i +='#'){
+
+    alert(i);
+
+}
+*/
+const ROUTER = {
+    usd: 'dollar',
+    blr: 'bel rouble',
+    rus: 'russian rouble',
+    eur: 'euro'
+}
+
+let a = prompt('Какую вы хотите выбрать валюту', '')
+
+let d = ROUTER[a]
+alert(d);
